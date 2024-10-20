c27cca7321cacd1c5c5734d6c8035463f12acc60
9x14S <Contact9x14S@proton.me>
2024-10-20 14:48:09 +0000

    Added backup feature

--- src/main.py
+++ src/main.py
@@ -11,28 +11,79 @@ __version__ = 0.1
 import argparse
 
 from pathlib import Path
+from shutil import copy
 from sys import exit, stderr
+from io import BytesIO
 
+from cairosvg import svg2png
 from PIL import Image
 
 
 def printerr(err_msg: str, exit_code: int) -> None:
+    """Prints and error message and (optionally) exits with error code."""
     print(err_msg, file=stderr)
     if exit_code > 0:
         exit(exit_code)
+    return
+
+
+def backup(files: list[Path]) -> None:
+    """Backs up all files in `files` and names them `<file>.bak`.
+    :param files: list of files to back up.
+    :type files: list[Path]
+
+    :return: None
+    """
+
+    for file in files:
+        try:
+            copy(file, str(file)+".bak")
+        except PermissionError as e:
+            printerr(str(e), 0)
+            printerr(f"File {file} could not be backed. Exiting.", 3)
+    return
 
 
 def main(
     watermark: str,
-    files: list,
+    files: list[str],
     position: str,
     alpha: float,
 ) -> int:
+    """Checks and loads all the files and the watermark. Then adds the watermark to 
+    them based on the options provided.
 
+    :param watermark: the watermark SVG filepath.
+    :type watermark: str
+
+    :param files: a list of length >= 1 of all the png/jpg files.
+    :type files: list[str]
+
+    :param position: the position where the watermark will be placed.
+    :type position: str
+
+    :param alpha: the desired transparency for the watermark.
+    :type alpha: float
+
+    :return: exit code
+    :rtype: int
+    """
+
+    # Check SVG
     if not watermark.lower().endswith(".svg"):
         printerr(f"File {watermark} not an SVG file.", 1)
+
+    watermark_path = Path(watermark)
+    if not watermark_path.exists():
+        printerr(f"Nonexistent file {watermark_path.name}. Exiting.", 2)
+
+    # Load SVG
+    svg2png_buffer = BytesIO()
+    svg2png(url=str(watermark_path), write_to=svg2png_buffer)
+    png_watermark = Image.open(fp=svg2png_buffer)
     
-    usable = []
+    # Check for valid files
+    usable: list[Path] = []
     for file in files:
         extension = file.lower().split('.')[-1]
         if extension not in ["jpeg", "jpg", "png"]:
@@ -44,7 +95,7 @@ def main(
             else:
                 usable.append(path)
 
-    print(usable)
+    backup(usable)
 
     return 0
 
@@ -68,4 +119,4 @@ if __name__ == "__main__":
     parser.add_argument("-a", "--alpha", default=100.0, type=float, help="transparency value")
     args = parser.parse_args()
 
-    exit(main(args.watermark, args.files, args.position, args.alpha))
+    printerr("Finalized execution", main(args.watermark, args.files, args.position, args.alpha))
